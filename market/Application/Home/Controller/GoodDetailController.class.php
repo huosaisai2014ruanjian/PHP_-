819ca909b9fffabef8997f9dad8271a01f5926f5
zhouboxue <fsyouzi@foxmail.com>
2016-12-19 16:17:32 +0800

    留言

--- market/Application/Home/Controller/GoodDetailController.class.php
+++ market/Application/Home/Controller/GoodDetailController.class.php
@@ -15,41 +15,77 @@ class GoodDetailController extends Controller
 {
         public function index(){
 
-            $where=I('get.id');//从商品列表页获取商品ID
-            //dump($where);exit;
-            $good = M('Goods')->where("id=$where")->find();//找到商品
-            $arr=explode(';',$good['photo']);//将图片URL分割 存到数组
-            $len=count($arr);
-            for ($j=0; $j < $len; $j++) {
-                $good ['image'][]=$arr[$j];
+//            $where=I('get');//从商品列表页获取商品Id;
+//            $goodsModel = M('Goods');
+//            $Message = M('Message');
+//            $good = M('Goods')->where($where)->find();//找到商品
+//            $arr=explode(';',$good['photo']);//将图片URL分割 存到数组
+//            $len=count($arr);
+//            for ($j=0; $j < $len; $j++) {
+//                $good ['image'][] = $arr[$j];
+//            }
+//            $sellid = $good['seller_id'][''];//卖家ID
+//            $user = M('users')->where("id=$sellid")->find();//dump($user);exit; //以卖家ID得到该卖家信息
+//
+//            $good1= $good['image']['1'];
+//            //dump($message);exit;
+//            $result=M('message')->join('market_goods on market_message.goods_id = market_goods.id')->join('market_users on market_message.fromuser_id = market_users.id')->where($where)
+//                ->field(array('market_message.time'=>'time',
+//                                'market_users.nickname'=>'nickname',
+//                                'market_users.head'=>'head',
+//                                'market_message.content'=>'content',
+//                                'market_goods.id'=>'goods_id',
+//                                'market_message.fromuser_id'=>'fromuser_id',
+//                                'market_message.touser_id'=>'touser_id',
+//                                'market_users.college'=>'college',
+//                                'market_message.belong_id'=>'belong_id',
+//                    ))->select();
+//            $Message =
+//            //dump($sellid);
+//            //dump($user);exit;
+//            dump($result);exit;
+//            //dump($good);exit;
+//            $this->assign('result',$result);
+//            $this->assign('user',$user);
+//            $this->assign('good',$good);//分好的数组 单个图片URL传到VIEW
+//            $this->assign('good1',$good1);
+            $where = I('get.');
+            $goodsModel = M('goods');
+            $usersModel = M('users');
+            $MessageModel = M('message');
+            $goods = $goodsModel->where($where)->find();
+            $imgarray=explode(';',$goods['photo']);//将图片URL分割 存到数组
+
+            $users = $usersModel->where($where)->find();
+
+            $where = array();
+            $where['goods_id'] = I('get.id');
+            $where['belong_id'] = 0;
+            $Message = $MessageModel->where($where)->select();
+            $i = 0;
+            foreach($Message as $key=>$value) {
+                    $result[$i]['content'] = $value['content'];
+                    $map['id'] = $value['fromuser_id'];
+                    $result[$i]['nickname'] = $usersModel->where($map)->getfield('nickname');
+                    $result[$key]['time'] = $value['time'];
+                    $where = array();
+                    $where['belong_id'] = $value['id'];
+                    $Messages = $MessageModel->where($where)->select();
+                    $j = 0;
+                    foreach($Messages as $key => $val){
+                        $map['id'] = $val['fromuser_id'];
+                        $Messages[$j]['fromnickname'] = $usersModel->where($map)->getfield('nickname');
+                        $map['id'] = $val['touser_id'];
+                        $Messages[$j]['tonickname'] = $usersModel->where($map)->getfield('nickname');
+                        $j = $j+1;
+                    }
+                    $result[$i]['temp'] = $Messages;
+                    $i = $i+1;
             }
-            $sellid = $good['seller_id'][''];//卖家ID
-            $user = M('users')->where("id=$sellid")->find();//dump($user);exit; //以卖家ID得到该卖家信息
-
-            $good1= $good['image']['1'];
-            //dump($message);exit;
-            $result=M('message')->join('market_goods on market_message.goods_id = market_goods.id')->join('market_users on market_message.fromuser_id = market_users.id')->where("goods_id = $where")
-                ->field(array('market_message.time'=>'time',
-                                'market_users.nickname'=>'nickname',
-                                'market_users.head'=>'head',
-                                'market_message.content'=>'content',
-                                'market_goods.id'=>'goods_id',
-                                'market_message.fromuser_id'=>'fromuser_id',
-                                'market_message.touser_id'=>'touser_id',
-                                'market_users.college'=>'college'
-                    ))->select();
-            //dump($sellid);
-            //dump($user);exit;
-            //dump($result);exit;
-            //dump($good);exit;
+            $this->assign('good',$goods);
+            $this->assign('img_url',$imgarray);
+            $this->assign('user',$users);
             $this->assign('result',$result);
-            $this->assign('user',$user);
-            $this->assign('good',$good);//分好的数组 单个图片URL传到VIEW
-            $this->assign('good1',$good1);
-
-
-
-
             $this->display();
 
         }
@@ -61,14 +97,14 @@ class GoodDetailController extends Controller
                 'goods_id' => I("post.goods_id"),
                 'fromuser_id' => I('post.fromuser_id'),
                 'touser_id' => I('post.fromuser_id'),
-                'belong_id' => '1',
+                'belong_id' => I('post.belong_id'),
             );
            //dump($data);exit;
             $comment = M("message"); // 实例化User对象
             if (1) {//条件
                 $add = $comment->add($data);
                 if ($add) {
-                    $this->success('评论成功');
+                    $this->success('留言成功',0);
                 } else {
                     $this->error('评论失败');
                 }
@@ -78,6 +114,8 @@ class GoodDetailController extends Controller
 
         }
         public function remessage(){
+
+
             $data = array(
                 'content' =>I("post.comment"),
                 'goods_id' =>I('post.goods_id'),
