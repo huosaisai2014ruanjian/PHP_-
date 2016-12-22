0bcd35f482cd6ae08d36a3b5251d44f7cea0b040
kdvnszrqu <384913085@qq.com>
2016-12-22 08:07:58 +0800

    缓存

--- market/Application/Runtime/Cache/Home/961bc35e09ef43b26948d521b77c2431.php
+++ market/Application/Runtime/Cache/Home/961bc35e09ef43b26948d521b77c2431.php
@@ -5,18 +5,18 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-<!--   <link href="/PHP_-/market/Public/dwz/themes/css/core.css" rel="stylesheet" type="text/css" /> -->
-  <link rel="stylesheet" type="text/css" href="/PHP_-/market/Public/lib/weui.css">
+<!--   <link href="/php_-/market/Public/dwz/themes/css/core.css" rel="stylesheet" type="text/css" /> -->
+  <link rel="stylesheet" type="text/css" href="/php_-/market/Public/lib/weui.css">
 
 
-  <link href="/PHP_-/market/Public/css/jquery-weui.min.css" type="text/css" rel="stylesheet"/>
-  <link rel="stylesheet" type="text/css" href="/PHP_-/market/Public/css/define.css">
-  <link href="/PHP_-/market/Public/css/base.css" rel="stylesheet" /> 
+  <link href="/php_-/market/Public/css/jquery-weui.min.css" type="text/css" rel="stylesheet"/>
+  <link rel="stylesheet" type="text/css" href="/php_-/market/Public/css/define.css">
+  <link href="/php_-/market/Public/css/base.css" rel="stylesheet" /> 
 
-<!-- 	<link href="/PHP_-/market/Public/lib/weui.min.css" type="text/css" rel="stylesheet"/>
-	<link href="/PHP_-/market/Public/css/jquery-weui.min.css" type="text/css" rel="stylesheet"/>
-	<link rel="stylesheet" type="text/css" href="/PHP_-/market/Public/css/define.css">
-  <link href="/PHP_-/market/Public/css/base.css" rel="stylesheet" />  -->
+<!-- 	<link href="/php_-/market/Public/lib/weui.min.css" type="text/css" rel="stylesheet"/>
+	<link href="/php_-/market/Public/css/jquery-weui.min.css" type="text/css" rel="stylesheet"/>
+	<link rel="stylesheet" type="text/css" href="/php_-/market/Public/css/define.css">
+  <link href="/php_-/market/Public/css/base.css" rel="stylesheet" />  -->
 	<style>
       .html,body{
         height: 100%;
@@ -36,7 +36,7 @@
 
 </head>
 <body>
-<div class="weui_search_bar" id="search_bar">
+<!-- <div class="weui_search_bar" id="search_bar">
   <form class="weui_search_outer">
     <div class="weui_search_inner">
       <i class="weui_icon_search"></i>
@@ -49,7 +49,7 @@
     </label>
   </form>
   <a href="javascript:" class="weui_search_cancel" id="search_cancel">取消</a>
-</div>
+</div> -->
 
 
 
@@ -60,31 +60,28 @@
   <section class="job-module"> 
   <dl class="retrie"> 
     <dt>
-      <a id="type" href="javascript:;">分类</a>
-      <a id="goods" href="javascript:;">商品</a>
-      <a id="school" href="javascript:;">学校</a>
-      <a id="price" href="javascript:;">价格</a>
+      <a id="type" href="javascript:;">一级分类</a>
+      <a id="goods" href="javascript:;">二级分类</a>
+      
     </dt> 
     <dd>
       <ul class="slide downlist">
-        <li><a href="#">不限</a></li>
-        <li><a href="/PHP_-/market/index.php/Home/goodslist/goodslist?id=1">学习物品</a></li>
-        <li><a href="/PHP_-/market/index.php/Home/goodslist/goodslist?id=2">电子数码</a></li>
-        <li><a href="/PHP_-/market/index.php/Home/goodslist/goodslist?id=4">服装服饰</a></li>
-        <li><a href="/PHP_-/market/index.php/Home/goodslist/goodslist?id=5">出行工具</a></li>
-        <li><a href="/PHP_-/market/index.php/Home/goodslist/goodslist?id=6">运动器材</a></li>
-        <li><a href="/PHP_-/market/index.php/Home/goodslist/goodslist?id=7">虚拟物品</a></li>
+       <!--  <li><a href="#">不限</a></li> -->
+        <li><a href="/php_-/market/index.php/Home/goodslist/goodslist?catid=1">学习物品</a></li>
+        <li><a href="/php_-/market/index.php/Home/goodslist/goodslist?catid=2">电子数码</a></li>
+        <li><a href="/php_-/market/index.php/Home/goodslist/goodslist?catid=3">服装服饰</a></li>
+        <li><a href="/php_-/market/index.php/Home/goodslist/goodslist?catid=4">出行工具</a></li>
+        <li><a href="/php_-/market/index.php/Home/goodslist/goodslist?catid=5">运动器材</a></li>
+        <li><a href="/php_-/market/index.php/Home/goodslist/goodslist?catid=6">虚拟物品</a></li>
       </ul>
     </dd>
 
     <dd class="goods"> 
-      <ul class="slide downlist"> 
-        <li><a href="#">四级</a></li> 
-        <li><a href="#">六级</a></li> 
-        <li><a href="#">考研</a></li> 
-      </ul> 
+     <ul class="slide downlist"> 
+      <?php if(is_array($category)): $i = 0; $__LIST__ = $category;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><li><a href="/php_-/market/index.php/Home/goodslist/getCatgory?id=<?php echo ($vo['id']); ?>"><?php echo ($vo['name']); ?></a></li><?php endforeach; endif; else: echo "" ;endif; ?>
+      </ul>  
     </dd> 
-    <dd class="school"> 
+    <!-- <dd class="school"> 
       <ul class="slide downlist"> 
         <li><a href="#">不限</a></li> 
         <li><a href="#">河北师大</a></li> 
@@ -97,16 +94,16 @@
         <li><a href="#">10元一下</a></li> 
         <li><a href="#">100以上</a></li> 
       </ul> 
-    </dd>  
+    </dd>   -->
   </dl> 
 </section> 
 
   <div class="weui_panel_bd">
    
 
-  <?php if(is_array($type)): $i = 0; $__LIST__ = $type;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><a href="/PHP_-/market/index.php/Home/GoodDetail/index?id=<?php echo ($vo['id']); ?>" class="weui_media_box weui_media_appmsg">
+  <?php if(is_array($type)): $i = 0; $__LIST__ = $type;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><a href="/php_-/market/index.php/Home/Gooddetail/index?id=<?php echo ($vo['id']); ?>" class="weui_media_box weui_media_appmsg">
       <div class="goodslist">
-        <img class="weui_media_appmsg_thumb" src="/PHP_-/market/Public/<?php echo ($vo["photo"]); ?>" height="100" width="100" alt="">
+        <img class="weui_media_appmsg_thumb" src="/php_-/market/Public/<?php echo ($vo["photo"]); ?>" height="100" width="100" alt="">
       </div>
       <div class="weui_media_bd">
       	<h4 class="weui_media_title"><?php echo ($vo["name"]); ?></h4>
@@ -117,7 +114,7 @@
 
       </div>
       <div style="text-align:right">
-      		<img src="/PHP_-/market/Public/<?php echo ($vo["head"]); ?>" style="height:40px;width=40px;" >
+      		<img src="/php_-/market/Public/<?php echo ($vo["head"]); ?>" style="height:40px;width=40px;" >
 
       		<p class="weui_media_desc"><?php echo ($vo["nickname"]); ?></p>
       		<p class="weui_media_desc"><?php echo ($vo["college"]); ?></p>
@@ -126,9 +123,9 @@
    
 </div>
 
-<script type="text/javascript" src="/PHP_-/market/Public/lib/jquery-2.1.4.js"></script>
-<script type="text/javascript" src="/PHP_-/market/Public/js/jquery-weui.min.js"></script>
-<script type="text/javascript" src="/PHP_-/market/Public/js/swiper.js"></script>
+<script type="text/javascript" src="/php_-/market/Public/lib/jquery-2.1.4.js"></script>
+<script type="text/javascript" src="/php_-/market/Public/js/jquery-weui.min.js"></script>
+<script type="text/javascript" src="/php_-/market/Public/js/swiper.js"></script>
 <script>
       $(".swiper-container").swiper({
         loop: true,
